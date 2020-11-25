ac7cd23b37f6c3913c660d8a6f4fbd14139ce87b
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2020-11-25 08:54:38 +0200

    Basic Service

--- angular/conFusion/src/app/menu/menu.component.ts
+++ angular/conFusion/src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -18,9 +19,10 @@ export class MenuComponent implements OnInit {
     this.selectedDish = dish;
   }
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
 }
