ee94b3635021314fb825b7be7927d4d0fd6efc94
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2020-12-02 12:34:31 +0200

    Configure Angular Application for JSON-Server

--- angular/conFusion/src/app/shared/baseurl.ts
+++ angular/conFusion/src/app/shared/baseurl.ts
@@ -1,1 +1,5 @@
-export const baseURL = 'https://localhost:3443/';
+// For the json-server
+export const baseURL = 'http://localhost:3000/';
+
+// For MongoDB
+//export const baseURL = 'https://localhost:3443/';
